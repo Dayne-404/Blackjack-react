58c965c832750e32a65539bea339cbf2e8ae2543
Dayne <dayne.dellaire@ontariotechu.net>
2024-04-11 13:13:13 -0400

    Started work on displaying cards

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,9 @@
+interface CardProps {
+    value: number;
+    suit: number;
+}
+
+function Card({value, suit}: CardProps) {
+}
+
+export default Card;
--- client/src/components/LargePlayerCard.tsx
+++ client/src/components/LargePlayerCard.tsx
@@ -2,15 +2,16 @@ import chad from "../assets/images/chad.jpg";
 
 function LargePlayerCard() {
     return(
+        <>
         <div className="large-player-card-container">
             <div className="player-hand-total"><p>[ TOTAL ]</p></div>
-            <p>[ CARDS ]</p>
             <div className="player-info">
                 <img className="player-photo" src={chad} alt="player photo" />
                 <h3>[ PLAYER NAME ]</h3>
                 <p>[ BET ]</p>
             </div>
         </div>
+        </>
     );
 }
 
