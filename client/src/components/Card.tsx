161efa6b04656b5f5d0823478f24e4d1f472d777
Dayne <dayne.dellaire@ontariotechu.net>
2024-04-11 21:08:10 -0400

    added cards to hand ui

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,9 +1,47 @@
+import aceDiamonds from '../assets/cards/ace_of_diamonds.svg?react';
+import twoDiamonds from '../assets/cards/2_of_diamonds.svg?react';
+import threeDiamonds from '../assets/cards/3_of_diamonds.svg?react';
+import fourDiamonds from '../assets/cards/4_of_diamonds.svg?react';
+import fiveDiamonds from '../assets/cards/5_of_diamonds.svg?react';
+import sixDiamonds from '../assets/cards/6_of_diamonds.svg?react';
+import sevenDiamonds from '../assets/cards/7_of_diamonds.svg?react';
+import eightDiamonds from '../assets/cards/8_of_diamonds.svg?react';
+import nineDiamonds from '../assets/cards/9_of_diamonds.svg?react';
+import tenDiamonds from '../assets/cards/10_of_diamonds.svg?react';
+import jackDiamonds from '../assets/cards/jack_of_diamonds.svg?react';
+import queenDiamonds from '../assets/cards/queen_of_diamonds.svg?react';
+import kingDiamonds from '../assets/cards/king_of_diamonds.svg?react';
+
+const cardsDict: { [key: string]: React.ComponentType } = {
+    '1D': aceDiamonds,
+    '2D': twoDiamonds,
+    '3D': threeDiamonds,
+    '4D': fourDiamonds,
+    '5D': fiveDiamonds,
+    '6D': sixDiamonds,
+    '7D': sevenDiamonds,
+    '8D': eightDiamonds,
+    '9D': nineDiamonds,
+    '10D': tenDiamonds,
+    'JD': jackDiamonds,
+    'QD': queenDiamonds,
+    'KD': kingDiamonds
+}
 interface CardProps {
-    value: number;
-    suit: number;
+    value: string
+    suit: string
+    style?: React.CSSProperties;
 }
 
 function Card({value, suit}: CardProps) {
+    const CardComponent = cardsDict[value + suit];
+    
+    if(!CardComponent)
+        return;
+
+    return (
+         <CardComponent />
+    )
 }
 
 export default Card;
--- client/src/components/LargePlayerCard.tsx
+++ client/src/components/LargePlayerCard.tsx
@@ -1,15 +1,38 @@
+import React, { useState } from 'react';
+import Card from "./Card";
 import chad from "../assets/images/chad.jpg";
 
 function LargePlayerCard() {
+    const [Cards, setCards] = useState<JSX.Element[]>([
+        <Card key="0" suit="D" value="K" />,
+        <Card key="1" suit="D" value="1" />,
+        <Card key="2" suit="D" value="2" />,
+    ])
+    
+    const largeHand = Cards.length > 3;
+    
+    const addCard = () => {
+        const newCard = <Card key={Cards.length} suit="D" value="3" />
+        setCards([...Cards, newCard]);
+    }
+
     return(
         <>
         <div className="large-player-card-container">
             <div className="player-hand-total"><p>[ TOTAL ]</p></div>
+            <div className={(largeHand ? "no-gap" : "small-gap") + " player-cards-container"}>
+                {Cards.map((card, index) => (
+                    <div key={index} className={(largeHand ? "negative-margins" : "no-margins") + " card-wrapper"}>
+                        {card}
+                    </div>
+                ))}
+            </div>
             <div className="player-info">
                 <img className="player-photo" src={chad} alt="player photo" />
                 <h3>[ PLAYER NAME ]</h3>
                 <p>[ BET ]</p>
             </div>
+            <button onClick={addCard}>Add Card</button>
         </div>
         </>
     );
